1e078011dccf15ccb32c7172edd3cdc1f8fa9a90
mota <thiagomotape@hotmail.com.br>
2023-04-06 19:32:40 -0300

    arrayList
    aprendendo como criar uma array para guardar as entidades do meu jogo

---
+++ logica_games/src/Entidade.java
@@ -0,0 +1,8 @@
+public class Entidade {
+
+    private int vida;
+    private int x, y;
+    public Entidade() {
+
+    }
+}
--- logica_games/src/Game.java
+++ logica_games/src/Game.java
@@ -1,11 +1,31 @@
+import java.util.ArrayList;
+
 //neste arquivo tem um loop de game basico que pode ser usado como exemplo para outros loops
 public class Game implements Runnable{ //implementei a interface runnable para fazer o loop do game
 
     private boolean isRunning; // variavel que vai ser usada pra dizer se o loop vai parar ou não
     private Thread thread;
+
+    //essa array guarda as entidades do meu game(personagens, objetos...)
+    private ArrayList<Entidade> entidades = new ArrayList<>();
+
+    public Game() {
+        //adicionei 5 entidades ao meu jogo
+        entidades.add(new Entidade());
+        entidades.add(new Entidade());
+        entidades.add(new Entidade());
+        entidades.add(new Entidade());
+        entidades.add(new Entidade());
+
+        //este loop mostra quantas entidades existem
+        for (int i = 0; i < entidades.size(); i++) {
+            System.out.println(i);
+        }
+    }
     public static void main(String[] args) {
         Game game = new Game(); //criando o game
         game.start(); //iniciando o game
+
     }
 
     //metodo que inicia o game
@@ -18,13 +38,13 @@ public class Game implements Runnable{ //implementei a interface runnable para f
     //metodo que vai atualizar o jogo
     public void tick() {
         //atualizar jogo
-        System.out.println("tick");
+        //System.out.println("tick");
     }
 
     //metodo que vai renderizar o jogo(imagens)
     public void render() {
         //renderizar jogo
-        System.out.println("render");
+        //System.out.println("render");
     }
     @Override //a interface runnable tras este metodo run
     public void run() {
