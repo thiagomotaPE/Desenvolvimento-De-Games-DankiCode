f13fe712d90661a150705218eccbe60a62312c4a
mota <thiagomotape@hotmail.com.br>
2023-04-06 19:17:08 -0300

    loop
    aqui criei o meu primeiro loop principal para games, o código esta todo comentado para saber oque cada coisa faz

--- logica_games/src/Game.java
+++ logica_games/src/Game.java
@@ -1,8 +1,43 @@
-public class Game {
+//neste arquivo tem um loop de game basico que pode ser usado como exemplo para outros loops
+public class Game implements Runnable{ //implementei a interface runnable para fazer o loop do game
+
+    private boolean isRunning; // variavel que vai ser usada pra dizer se o loop vai parar ou não
+    private Thread thread;
     public static void main(String[] args) {
-        while(true) {
-            logica();
-            renderizar();
+        Game game = new Game(); //criando o game
+        game.start(); //iniciando o game
+    }
+
+    //metodo que inicia o game
+    public synchronized void start() {
+        isRunning = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    //metodo que vai atualizar o jogo
+    public void tick() {
+        //atualizar jogo
+        System.out.println("tick");
+    }
+
+    //metodo que vai renderizar o jogo(imagens)
+    public void render() {
+        //renderizar jogo
+        System.out.println("render");
+    }
+    @Override //a interface runnable tras este metodo run
+    public void run() {
+        while(isRunning) {
+            tick();
+            render();
+            //como deixar o jogo um pouco mais lendo, neste caso com 60 fps(este não é o melhor jeito de ser feito)
+//            try {
+//                Thread.sleep(1000/60);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
         }
+
     }
 }
