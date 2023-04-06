1fc649b589e8fd853badcef32a1a565285e6cc68
mota <thiagomotape@hotmail.com.br>
2023-04-06 18:51:55 -0300

    basico
    falando sobre a logica de games

--- logica_games/src/Game.java
+++ logica_games/src/Game.java
@@ -1,6 +1,8 @@
 public class Game {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        while(true) {
+            logica();
+            renderizar();
+        }
     }
 }
